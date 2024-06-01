78f89f29ee5825aaecc7473e123d74da2fe0c96d
yaoyi <yyaome@hotmail.com>
2024-06-01 16:26:41 -0400

    Added SharePreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,16 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -57,93 +33,52 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // var _counter = 0.0;
   var myFontSize = 30.0;
-  late TextEditingController _controller;
+  late TextEditingController _controller1;
+  late TextEditingController _controller2;
   var imageSource = "images/question-mark.png";
+  late SharedPreferences savedData;
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController();
+    _controller1 = TextEditingController();
+    _controller2 = TextEditingController();
+    //Load the file for SharedPreferences, and pass it by the then() lambda function to savedData
+    SharedPreferences.getInstance().then( (result){
+      savedData = result;
+      var loginName = result.getString("Login");
+      if(loginName != null){
+        _controller1.text = loginName;
+      }
+    });
+
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    _controller1.dispose();
+    _controller2.dispose();
   }
-
-  void setNewValue(double value)
-  {
-    setState(() {
-      // _counter = value;
-      myFontSize = value;
-    });
-  }
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     // if(_counter < 99.0)
-  //     //   _counter++;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Text(
-            //   'You have pushed the button this many times:',
-            //   style: TextStyle(fontSize: myFontSize),
-            // ),
-            // Text(
-            //   '$_counter',
-            //   style: TextStyle(fontSize: myFontSize),
-            // ),
-            // Slider(value:_counter, max:100.0, onChanged: setNewValue, min:0.0)
-            const TextField(decoration: InputDecoration(
+            TextField(controller: _controller1,
+                decoration: InputDecoration(
                               hintText:"Type here",
                               border: OutlineInputBorder(),
                               labelText: "Login"
                   )),
-            TextField(controller: _controller, obscureText: true,
+            TextField(controller: _controller2, obscureText: true,
                 decoration: const InputDecoration(
                     hintText:"Type here",
                     border: OutlineInputBorder(),
@@ -151,26 +86,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 )),
             ElevatedButton(
                 onPressed: buttonClicked,
-                //     () {
-                //   setState(() {
-                //     var myText = _controller.value.text;
-                //     if (myText == "QWERTY123") {
-                //       imageSource = "images/idea.png";
-                //     } else {
-                //       imageSource = "images/stop.png";
-                //     }
-                //   });
-                // },
                 child: const Text("Login", style: TextStyle(fontSize: 25, color: Colors.blue))),
             Image.asset(imageSource, width:200, height:200)
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -182,10 +102,17 @@ class _MyHomePageState extends State<MyHomePage> {
     showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
-        title: const Text('Alert'),
+        title: const Text('Save data'),
         content: const Text('Do you want to save your username and password?'),
         actions: <Widget>[
-              ElevatedButton(onPressed: (){Navigator.pop(context);}, child: Text("OK")),
+              ElevatedButton(
+                  onPressed: (){
+                    var login = _controller1.value.text;
+                    var password = _controller2.value.text;
+                    savedData.setString("Login", login);
+                    Navigator.pop(context);
+                    },
+                  child: Text("OK")),
               FilledButton(onPressed: (){}, child: Text("Cancel"))
         ],
       ),
