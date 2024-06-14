803134f489ebf2bd70c0f800e57b5e973337ac5b
yaoyi <yyaome@hotmail.com>
2024-06-13 21:06:17 -0400

    Lab6 completed - ListView

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,121 +32,118 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // var _counter = 0.0;
-  var myFontSize = 30.0;
+  var isChecked = false;
   late TextEditingController _controller;
-  var imageSource = "images/question-mark.png";
+  late TextEditingController _controller2;
 
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
+    _controller2 = TextEditingController();
   }
 
   @override
   void dispose() {
-    super.dispose();
     _controller.dispose();
+    _controller2.dispose();
+    super.dispose();
   }
-
-  void setNewValue(double value)
-  {
-    setState(() {
-      // _counter = value;
-      myFontSize = value;
-    });
-  }
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     // if(_counter < 99.0)
-  //     //   _counter++;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Text(
-            //   'You have pushed the button this many times:',
-            //   style: TextStyle(fontSize: myFontSize),
-            // ),
-            // Text(
-            //   '$_counter',
-            //   style: TextStyle(fontSize: myFontSize),
-            // ),
-            // Slider(value:_counter, max:100.0, onChanged: setNewValue, min:0.0)
-            const TextField(decoration: InputDecoration(
-                              hintText:"Type here",
-                              border: OutlineInputBorder(),
-                              labelText: "Login"
-                  )),
-            TextField(controller: _controller, obscureText: true,
-                decoration: const InputDecoration(
-                    hintText:"Type here",
-                    border: OutlineInputBorder(),
-                    labelText: "Password",
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    var myText = _controller.value.text;
-                    if (myText == "QWERTY123") {
-                      imageSource = "images/idea.png";
-                    } else {
-                      imageSource = "images/stop.png";
-                    }
-                  });
+          children: [
+            Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(//To contain the TextField and Button in the same row
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        hintText: "Type here",
+                        border: OutlineInputBorder(),
+                        labelText: "Enter a to-do item",
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: const Text("Add"),
+                    onPressed: () {
+                      setState(() {
+                        words.add(_controller.text);
+                        _controller.clear();
+                      });
+                    }),
+                ]),
+            ),
+
+            //The if statement to display the text when the array is empty
+            if (words.isEmpty)
+              const Text(
+                  "There are no items in the list",
+                  style: TextStyle(fontSize: 20),
+              ),
+            // Use Expanded to contain ListView in the Column container, so it can take as much space as possible
+            Expanded(
+              child: ListView.builder(
+                itemCount: words.length,
+                itemBuilder: (context, rowNum) {
+                  return GestureDetector(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Row number:  $rowNum",
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                          )),
+                        Text(
+                          words[rowNum],
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                          )),
+                      ]),
+                    onLongPress: () {
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Delete Item'),
+                          content: const Text('Do you want to delete this to-do item?'),
+                          actions: <Widget>[
+                            ElevatedButton(onPressed: () {
+                                setState(() {
+                                  words.removeAt(rowNum);
+                                });
+                                Navigator.pop(context);
+                                },
+                                child: const Text("Yes")
+                            ),
+                            ElevatedButton(onPressed: () {
+                                Navigator.pop(context);
+                                },
+                                child: const Text("No")
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
                 },
-                child: const Text("Login", style: TextStyle(fontSize: 25, color: Colors.blue))),
-            Image.asset(imageSource, width:200, height:200)
-          ],
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+              )),
+          ]),
+      ));
   }
+
+  List<String> words = [ ];
+  // var wordsArray = <String>[ ]; //Another way to create a array
+
 }
